957d355ea64ce872c319b89285910d29ffa72843
felixokolo <pheelix.info@gmail.com>
2022-06-04 15:46:25 +0000

    corrections

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elem, idx in zip(row, range(len(row))):
-            print("{}".format(elem), end='')
+            print("{:d}".format(elem), end='')
             if idx < len(row)-1:
                 print('', end=' ')
         print('')
