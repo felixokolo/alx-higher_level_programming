e3bdd1bbbba0be24195faea59b3846b35d343402
felixokolo <pheelix.info@gmail.com>
2022-06-20 16:06:36 +0100

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end = "")
+        except IndexError:
+            return c
+        else:
+            c += 1
+    else:
+        print("")
+    return c
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end = "")
+        except IndexError as e:
+            print(traceback.format_exc())
+        except Exception:
+            continue
+        else:
+            c += 1
+    else:
+        print("")
+    return c
