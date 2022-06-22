f39098b3c42b2929deca70014d5d7dbff4af3b43
felixokolo <pheelix.info@gmail.com>
2022-06-22 09:08:55 +0100

    addition

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+Square = __import__('101-square').Square
 
-MagicClass = __import__('103-magic_class').MagicClass
-a = MagicClass(6)
-print(a.area())
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
