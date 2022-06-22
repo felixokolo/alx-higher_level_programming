3ddb471cf2b2c854fd1e17acbe32c5bab6dd6d1a
felixokolo <pheelix.info@gmail.com>
2022-06-22 08:02:34 +0100

    addition

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import math
+"""Defining MagicClass class"""
+
+
+class MagicClass:
+    """Practice empty class
+
+    Attributes:
+        __radius (int): Private size of radius attribute
+
+    """
+    def __init__(self, radius=0):
+        """ defining the init functiom
+
+        Args:
+            radius (int): for initializing the radius attribute
+
+        """
+        if isinstance(radius, int) or  isinstance(radius, float):
+            self.__radius = radius
+        else:
+            self.__radius = None
+            raise TypeError("radius must be a number")
+
+    def area(self):
+        """ Method for calculating area of square"""
+
+        return math.pi * self.__radius ** 2
+
+    def circumference(self):
+        """ Method for calculating circumference of square"""
+
+        return math.pi * self.__radius * 2
