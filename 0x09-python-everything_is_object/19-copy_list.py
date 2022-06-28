5a31e09ab06c85c89c4750c2ef82a16b7de49092
felixokolo <pheelix.info@gmail.com>
2022-06-28 19:56:49 +0000

    correction

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-"""Practice function definition"""
-
-
 def copy_list(lst):
-    """Function that returns a copy of a list
-    Args:
-    l (list): List to make copy
-    """
     return lst[:]
