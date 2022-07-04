b1384a3c1df424e0315b46a6df132500b072d8e9
felixokolo <pheelix.info@gmail.com>
2022-07-04 20:59:20 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function module"""
+
+
+def lookup(obj):
+    """ Lookup function that returns a list of attr"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List inheritance module"""
+
+
+class MyList(list):
+    """Modified List class
+    Attributes:
+        """
+    def print_sorted(self):
+        """Prints a list in sorted form"""
+        if self.__len__ == 0:
+            print([])
+            return
+        new = []
+        for i in set(self):
+            for j in self:
+                if j == i:
+                    new.append(j)
+        print(new)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/1-my_list.txt")
