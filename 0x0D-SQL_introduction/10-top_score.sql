3209720cb1836ef03b80b087012b4a54e88de5e4
felixokolo <pheelix.info@gmail.com>
2022-08-11 19:27:46 +0000

    correction to 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- A script that lists all records of the table second_table
 -- of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM second_table
-ORDER BY second_table.score DESC;
+SELECT s.score, s.name  FROM second_table as s
+ORDER BY s.score DESC;
