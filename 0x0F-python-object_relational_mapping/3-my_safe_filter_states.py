ad685f6d9b2feedd09a0ba6e6ebeae0b08d2d72d
felixokolo <pheelix.info@gmail.com>
2022-09-09 21:56:47 +0000

    correction

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          db=sys.argv[3])
     cur = db.cursor()
     query = 'SELECT * FROM states ' + 'WHERE name = \'{}\''.format(sys.argv[4])
-    if ('states' not in query):
+    if (query.count('states') == 1):
         r = cur.execute(query)
         rows = cur.fetchall()
         for row in rows:
