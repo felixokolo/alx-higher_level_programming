891569e1e5ebeeb0480a0c152c52d7f33f6cd5a3
felixokolo <pheelix.info@gmail.com>
2022-09-09 21:23:19 +0000

    correction

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,12 +9,10 @@ if __name__ == '__main__':
             user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     query = 'SELECT * FROM states ' + 'WHERE name = \'{}\''.format(sys.argv[4])
-    if ('states' in query):
-		r = cur.execute(query)
-		rows = cur.fetchall()
-		for row in rows:
-			print(row)
-	else:
-		print('Injection')
+    if ('states' not in query):
+        r = cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
     cur.close()
     db.close()
