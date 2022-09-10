2796e6f0714be563fc37d520670476705495b21f
felixokolo <pheelix.info@gmail.com>
2022-09-10 21:10:46 +0000

    corrections

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
                          user=sys.argv[1], password=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = 'SELECT c.id, c.name, s.name FROM cities AS c ' +
-            'JOIN states as s' +
-            'WHERE c.state_id = s.id' +
-            'ORDER BY c.id'
+    query = ('SELECT c.id, c.name, s.name FROM cities AS c ' +
+             'JOIN states as s ' +
+             'WHERE c.state_id = s.id ' +
+             'ORDER BY c.id')
     r = cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,19 +11,24 @@ if __name__ == '__main__':
                          user=sys.argv[1], password=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    query = '(SELECT c.id, c.name as c_name, ' +
-            's.name as s_name FROM cities AS c ' +
-            'JOIN states as s ' +
-            'WHERE c.state_id = s.id ' +
-            'ORDER BY c.id) AS joined ' +
-            'SELECT c_name FROM joined AS j ' +
-            'WHERE LOWER(j.s_name) = LOWER(\'{}\'})'.
-            format(sys.argv[4])
+    query = ('SELECT c_name FROM ' +
+             '(SELECT c.id, c.name as c_name, ' +
+             's.name as s_name FROM cities AS c ' +
+             'JOIN states as s ' +
+             'WHERE c.state_id = s.id ' +
+             'ORDER BY c.id) j ' +
+             'WHERE LOWER(j.s_name) = LOWER(\'{}\')'.
+             format(sys.argv[4]))
     if (query.count('states') == 1 and
         query.count('cities') == 1):
         r = cur.execute(query)
         rows = cur.fetchall()
+        res = ''
         for row in rows:
-            print(row)
+            for col in row:
+                res += col
+                res += ', '
+        print(res[0:-2])
+
     cur.close()
     db.close()
