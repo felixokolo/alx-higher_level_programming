0c79867cb3f1e4e03b537d478be010f54e45b301
felixokolo <pheelix.info@gmail.com>
2022-09-11 16:35:45 +0000

    correction

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2],
                                   sys.argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)()
-    res = session.query(State).filter_by(id=1).first()
+    res = session.query(State).first()
     print('{}: {}'.format(res.id, res.name))
