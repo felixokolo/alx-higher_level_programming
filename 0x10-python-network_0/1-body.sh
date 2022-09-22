2fcd0bc8935ffc1a91e5a257e8b8a3a5fd83c04c
felixokolo <pheelix.info@gmail.com>
2022-09-22 22:44:50 +0000

    correction

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Gets the size of the body
-response=$(curl -sI "$1")
-c=$(echo $response | grep 'HTTP' | awk '{print $2}')
-if ((c == 200))
-then
-	curl -s "$1" -X GET -L 
-fi
+# Gets body content
+curl -s "$1" -X GET -L 
