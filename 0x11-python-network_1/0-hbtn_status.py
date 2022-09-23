b9862323f3fb8655aafc7804bea6dbce84c25056
felixokolo <pheelix.info@gmail.com>
2022-09-23 13:26:20 +0000

    coreection

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
             'https://alx-intranet.hbtn.io/status')\
             as response:
         html = response.read()
-    print("Body response:\n\t- type: {}\
-          \n\t- content: {}\
-          \n\t- utf8 content: {}".
+    print(("Body response:\n\t- type: {}\n" +
+           "\t- content: {}\n" +
+           "\t- utf8 content: {}").
           format(type(html),
                  html,
                  html.decode("utf8", "replace")))
