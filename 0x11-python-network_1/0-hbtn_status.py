ae3fc1e83267121147d0fc8ccacc68613ea671e3
felixokolo <pheelix.info@gmail.com>
2022-09-23 11:18:22 +0000

    correction

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status')\
+            as response:
+        html = response.read()
+    print("Body response:\n\t- type: {}\
+          \n\t- content: {}\
+          \n\t- utf8 content: {}".
+          format(type(html),
+                 html,
+                 html.decode("utf8")))
