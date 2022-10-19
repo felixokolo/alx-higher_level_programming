d5650d0162c6dc02294e12ad4014e7831e3450c0
felixokolo <pheelix.info@gmail.com>
2022-10-19 20:55:57 +0000

    correction

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// A script to get status code
+const request = require('request');
+const base = process.argv[2];
+const id = '18';
+let res;
+let count = 0;
+request.get(base + '/?format=json', (error, response, body) => {
+  if (response && !error) {
+    res = JSON.parse(body);
+    let i = 0;
+    while (i < res.results.length) {
+      let j = 0;
+      while (j < res.results[i].characters.length) {
+        if (res.results[i].characters[j].includes(id)) {
+          count = count + 1;
+        }
+        j++;
+      }
+      i++;
+    }
+    console.log(count);
+  }
+});
