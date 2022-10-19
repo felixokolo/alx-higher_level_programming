780d20b6e15978564a7d4b8a7dde4b382a17f658
felixokolo <pheelix.info@gmail.com>
2022-10-19 09:39:39 +0000

    correction

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 const getURL = process.argv[2];
 request.get(getURL).on('response', (response) => {
   if (response) {
